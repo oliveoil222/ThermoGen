171adbfd68805231f692fa942f35b1dd99ad9c10
Gareth Carew <50182665+GarethCarew@users.noreply.github.com>
2025-10-11 21:17:13 -0700

    Temp Read&Write
    Moved functionality from notification file  & UI file to main file.
    
    Added temp read & write

--- TestDemo/temperature_read.py
+++ TestDemo/temperature_read.py
@@ -1,22 +1,34 @@
 import nidaqmx
 from nidaqmx.stream_readers import AnalogMultiChannelReader
 import numpy as np
+import threading
 
-with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_thrmcpl_chan("Dev1/ai0", min_val=-60, max_val=60)
-    task.ai_channels.add_ai_thrmcpl_chan("Dev1/ai1", min_val=-60, max_val=60)
-    task.ai_channels.add_ai_thrmcpl_chan("Dev1/ai2", min_val=-60, max_val=60)
+class TemperatureReader:
+    def __init__(self):
+        self.task = nidaqmx.Task()
+        self.reader = None
+        self.data = None
+        self.thread = threading.Thread(target=self.start_read)
+        self.thread.daemon = True
+        self.thread.start()
 
-    task.timing.cfg_samp_clk_timing(rate=8, sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS, samps_per_chan=100)
 
-    reader = AnalogMultiChannelReader(task.in_stream)
-    data = np.empty((3,))  # 2 channels, 100 samples per read
+    def start_read(self):
+        with self.task as task:
+            task.ai_channels.add_ai_thrmcpl_chan("Dev1/ai0", min_val=-60, max_val=60)
+            task.ai_channels.add_ai_thrmcpl_chan("Dev1/ai1", min_val=-60, max_val=60)
+            task.ai_channels.add_ai_thrmcpl_chan("Dev1/ai2", min_val=-60, max_val=60)
 
-    task.start()
-    try:
-        while True:
-            reader.read_one_sample(data, timeout=10.0)
-            print(data)
-            # Add your processing code here
-    except KeyboardInterrupt:
-        pass
+            task.timing.cfg_samp_clk_timing(rate=8, sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS, samps_per_chan=100)
+
+            self.reader = AnalogMultiChannelReader(task.in_stream)
+            self.data = np.empty((3,))  # 3 channels
+
+            task.start()
+
+            try:
+                while True:
+                    self.reader.read_one_sample(self.data, timeout=10.0)
+                    # Add your processing code here
+            except KeyboardInterrupt:
+                pass
